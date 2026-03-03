5ca9b5cbc7ce6d3ab9675f15a411f1ca3ff7942b
HaaaamBurger <gemasclashes@gmail.com>
2026-03-03 09:01:36 +0200

    init lib

---
+++ src/main/java/com/vendo/test_utils_lib/AssertionUtils.java
@@ -0,0 +1,54 @@
+package com.vendo.test_utils_lib;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Logger;
+
+public class AssertionUtils {
+
+    private static final Logger LOGGER = Logger.getLogger(AssertionUtils.class.getName() );
+
+    public static void assertFromDto(Object entity, Object dto) {
+        Map<String, Object> entityData = mapObject(entity);
+        Map<String, Object> dtoData = mapObject(dto);
+
+        for (Map.Entry<String, Object> entry : dtoData.entrySet()) {
+            Object actual = entityData.get(entry.getKey());
+
+            if (!entityData.containsKey(entry.getKey())) {
+                throw new IllegalArgumentException(
+                        "Missing field in entity: " + entry.getKey()
+                );
+            }
+
+            if (!Objects.equals(entry.getValue(), actual)) {
+                throw new IllegalArgumentException(
+                        "Mismatch for field: " + entry.getKey() +
+                                ", expected: " + entry.getValue() +
+                                ", actual: " + actual
+                );
+            }
+        }
+    }
+
+    private static Map<String, Object> mapObject(Object o) {
+        Map<String, Object> data = new HashMap<>();
+
+        Class<?> dtoClass = o.getClass();
+        Field[] fields = dtoClass.getDeclaredFields();
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+
+            try {
+                data.put(field.getName(), field.get(o));
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException("Unmapped target value: " + field.getName());
+            }
+        }
+
+        return data;
+    }
+}
